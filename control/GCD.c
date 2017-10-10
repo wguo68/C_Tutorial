917a89644271d99edf98cf4f6cd4ec442f784861
wguo68 <wguo68@gmail.com>
2017-10-10 10:02:10 +0800

    Update GCD.c

--- control/GCD.c
+++ control/GCD.c
@@ -8,3 +8,16 @@ void main(){
 	}
 	printf("GCD is %d\n",m);
 }
+
+void main(){
+	int m,n;
+	while(1){
+		scanf("%d %d",&m,&n);
+
+		while(n){
+		  int k = m%n;
+		  m = n; n = k;
+	   }	
+	   printf("GCD is %d\n",m);
+	}
+}
